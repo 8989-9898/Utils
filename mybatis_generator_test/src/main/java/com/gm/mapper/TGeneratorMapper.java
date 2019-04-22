7ee9d613a60d6a31eb364c49bf2a9ac0f5a33fd7
8989-9898 <https://github.com/8989-9898/Utils.git>
2019-04-22 16:45:50 +0800

    测试时的修改

---
+++ mybatis_generator_test/src/main/java/com/gm/mapper/TGeneratorMapper.java
@@ -0,0 +1,45 @@
+package com.gm.mapper;
+
+import com.gm.entry.TGenerator;
+import java.util.List;
+
+public interface TGeneratorMapper {
+    /**
+     * @Param: java.lang.Integer id
+     * @Date: 2019/04/22
+     * @Description: 
+     * @Return: int
+     */
+    int deleteByPrimaryKey(Integer id);
+
+    /**
+     * @Param: com.gm.entry.TGenerator record
+     * @Date: 2019/04/22
+     * @Description: 
+     * @Return: int
+     */
+    int insert(TGenerator record);
+
+    /**
+     * @Param: java.lang.Integer id
+     * @Date: 2019/04/22
+     * @Description: 
+     * @Return: com.gm.entry.TGenerator
+     */
+    TGenerator selectByPrimaryKey(Integer id);
+
+    /**
+     * @Date: 2019/04/22
+     * @Description: 
+     * @Return: java.util.List<com.gm.entry.TGenerator>
+     */
+    List<TGenerator> selectAll();
+
+    /**
+     * @Param: com.gm.entry.TGenerator record
+     * @Date: 2019/04/22
+     * @Description: 
+     * @Return: int
+     */
+    int updateByPrimaryKey(TGenerator record);
+}
